26047a97cf5629177960d1d51a51574a49686dc6
dineshpolaru007 <119536488+dineshpolaru007@users.noreply.github.com>
2022-12-01 01:27:03 +0530

    Proposed changes

--- BasicDataStructure/Algorithm/Search/test.java
+++
@@ -1,9 +0,0 @@
-package dataStructure.BasicDataStructure.Algorithm.Search;
-
-public class test {
-
-    public static void main(String[] args) {
-        System.out.println(0%2);
-        System.out.println(1%2);
-    }
-}
